c485a7c964f68a940c1ff5a3439d7175cc27e5c1
Болат Берд <rpyz72@mail.ru>
2025-10-18 19:27:31 +0500

    @BolatBerd (Homework №4):  error correctionJS_2
    Исправление ошибок

--- main.js
+++ main.js
@@ -1,39 +1,39 @@
 const productCard = document.querySelector('.card');
 const productCards = document.querySelectorAll('.card');
-const chengeColorCardBtn = document.querySelector('#change-color-card-btn');
-const chengeColorCardsBtn = document.querySelector('#change-color-cards-btn');
+const changeColorCardBtn = document.querySelector('#change-color-card-btn');
+const changeColorCardsBtn = document.querySelector('#change-color-cards-btn');
 const openGoogleBtn = document.querySelector('#open-google');
 const blueColorHash = '#9dbff3ff';
 const greenColorHash = '#d5ddd5ff';
-const GoogleURL = 'https://google.com';
+const googleURL = 'https://google.com';
 
-chengeColorCardBtn.addEventListener('click', () => {
+changeColorCardBtn.addEventListener('click', () => {
   productCard.style.backgroundColor = blueColorHash;
 })
-chengeColorCardsBtn.addEventListener('click', () => {
+changeColorCardsBtn.addEventListener('click', () => {
   productCards.forEach((card) => 
   card.style.backgroundColor = greenColorHash)
 })
 openGoogleBtn.addEventListener('click', openGoogle)
 
-function openGoogle(){
+function openGoogle() {
   const answer = confirm('Вы действительно хотите открыть Google?');
-  if (answer === true){
-    window.open(GoogleURL)
+  if (answer === true) {
+    window.open(googleURL)
   } else {
     return;
   }
 }
 
-const titleText = document.querySelector('.heading');
-const textContent = titleText.textContent;
+const title = document.querySelector('.heading');
+const headerContent = title.textContent;
 
-titleText.addEventListener('mouseover', () => {
-  console.log(textContent);
+title.addEventListener('mouseover', () => {
+  console.log(headerContent);
 });
 
-const changeColorBtn = document.querySelector('.change-color-btn');
+const changeColorBtn = document.querySelector('.bg-aquamarine');
 
 changeColorBtn.addEventListener('click', () => {
-  changeColorBtn.classList.toggle('change-color-btn-red')
+  changeColorBtn.classList.toggle('bg-red')
 })
